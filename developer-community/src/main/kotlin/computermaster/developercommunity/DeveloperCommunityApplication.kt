e50cd14fd9445805c3478d8bf13f3edb6f9d952a
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2022-03-26 17:58:33 +0900

    Start Spring Project(스프링 서버)

---
+++ developer-community/src/main/kotlin/computermaster/developercommunity/DeveloperCommunityApplication.kt
@@ -0,0 +1,11 @@
+package computermaster.developercommunity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeveloperCommunityApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeveloperCommunityApplication>(*args)
+}
---
+++ developer-community/src/test/kotlin/computermaster/developercommunity/DeveloperCommunityApplicationTests.kt
@@ -0,0 +1,13 @@
+package computermaster.developercommunity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeveloperCommunityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
