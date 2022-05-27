1e26baefa924038f1b725208c6ac531f054984bb
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2022-05-28 05:57:06 +0900

    add board - PRG패턴 적용

--- developer-community/src/main/kotlin/computermaster/developercommunity/web/BoardsController.kt
+++ developer-community/src/main/kotlin/computermaster/developercommunity/web/BoardsController.kt
@@ -2,20 +2,31 @@ package computermaster.developercommunity.web
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.mvc.support.RedirectAttributes
 
 @Controller
 @RequestMapping("/boards")
 class BoardsController {
 
-    @GetMapping()
+    @GetMapping
     fun boardList(): String{
         return "boards"
     }
 
     @GetMapping("/new")
     fun addBoard(): String{
+        println("get method")
         return "addboard"
+    }
+
+    @PostMapping("/new")
+    fun createBoard(): String{
+        //새로운 보드 추가하는 로직
+        println("post method")
+        //PRG 패턴 적용
+        return "redirect:/boards"
     }
 
 }
